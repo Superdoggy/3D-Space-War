c82034f4532ecd8bd0c4c0060b21076552c597ce
Superdoggy <superdoggy.pr0gr4mm3r@gmail.com>
2015-08-17 17:36:49 -0400

    Update spacewar.js

--- spacewar.js
+++ spacewar.js
@@ -7,6 +7,27 @@ var init = function() {
   }
   catch(e) {}
 }
+var daeloader = new THREE.ColladaLoader();
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+var renderer = new THREE.WebGLRenderer();
+
+var setup = function() {
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  document.body.appendChild( renderer.domElement );
+
+  camera.position.z = 5;
+  daeloader.load('https://github.com/Superdoggy/3D-Space-War/blob/gh-pages/TestSphere.dae', function(collada){
+    scene.add(collada.scene); //So... does this add the model itself to the scene? I'm assuming collada is the model. Hmm.
+    render();
+  })
+}
+var render = function () {
+  requestAnimationFrame( render );
+
+	renderer.render(scene, camera);
+};
+setup();
 
 $(document).ready(function(){
   init();
